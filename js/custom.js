321ebf6aaf4f2a12890faac8ae90ea1cddffd58c
Varulv1997 <farahianas5@gmail.com>
2020-11-11 15:56:35 +0100

    strings removed -r1

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,3 @@
-/*
-Template Name: Osahan Property - Bootstrap 4 Light Real Estate Theme
-Author: Askbootstrap
-Author URI: https://themeforest.net/user/askbootstrap
-Version: 1.0
-*/
 $(document).ready(function() {
 	"use strict";
 
@@ -15,6 +9,6 @@ $(document).ready(function() {
 		$(document).ready(function() {
 			$('.select2').select2();
 		});
-	var imported = document.createElement('script');imported.src = 'https://www.googletagmanager.com/gtag/js?id=UA-120909275-1';document.head.appendChild(imported);window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-120909275-1');
+	var imported = document.createElement('script');imported.src = 'https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXX';document.head.appendChild(imported);window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-XXXXXXX-1');
 });
